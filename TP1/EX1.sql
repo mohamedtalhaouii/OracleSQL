c2e184e2d5925fa582ec76cb2e41fe546bf8e5ad
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2025-04-02 18:14:41 +0000

    AJT TP1

---
+++ TP1/EX1.sql
@@ -0,0 +1,27 @@
+--1) Installation d’Oracle Database Express Edition 11g :
+---> (https://www.oracle.com/database/technologies/xe-prior-release-downloads.html)
+
+
+--2) Installation de SQL Developer :
+---> (https://www.oracle.com/fr/database/sqldeveloper/technologies/download)
+
+
+--3) Lier Oracle Database avec SQL Developer :
+--    Nom de connexion : OracleDB
+--    Nom d’utilisateur : SYSTEM
+--    Mot de passe : SYSTEM
+--    Type de connexion : Basique
+--    Nom d’hôte : localhost
+--    Port : 1521
+--    SID : XE
+--> Tester et enregistrer la connexion
+
+
+--4) Création d’un nouvel utilisateur :
+CREATE USER mohamed_talhaoui IDENTIFIED BY mot_pass;
+
+
+--5) Attribuer les droits à l'utilisateur :
+GRANT CONNECT, RESOURCE TO mohamed_talhaoui;
+
+
---
+++ TP1/EX2.sql
@@ -0,0 +1,48 @@
+--1---------------------------------------------------------------------------------
+CREATE USER useroracle1 IDENTIFIED BY uoracle1;
+
+--2---------------------------------------------------------------------------------
+CONNECT useroracle1/uoracle1;
+-- La connexion échoue avec une erreur du type "ORA-01045: user USERORACLE1 lacks CREATE SESSION privilege; logon denied".
+-- Pourquoi ? Parce que l'utilisateur n'a pas encore le privilège CREATE SESSION nécessaire pour se connecter à la base.
+
+--3---------------------------------------------------------------------------------
+CREATE USER useroracle2 IDENTIFIED BY uoracle2;
+
+--4---------------------------------------------------------------------------------
+GRANT CREATE TABLE TO useroracle1;
+
+--5---------------------------------------------------------------------------------
+GRANT CREATE SESSION, CREATE TABLE TO useroracle2;
+
+--6---------------------------------------------------------------------------------
+CONNECT useroracle2/uoracle2; -- Se connecter avec useroracle2
+
+CREATE TABLE Dept(
+  NumDept NUMBER,
+  NomDept VARCHAR2(50),
+  Localisation VARCHAR2(50) 
+);
+
+--7---------------------------------------------------------------------------------
+GRANT SELECT ON Dept TO useroracle1;
+
+--8---------------------------------------------------------------------------------
+GRANT UPDATE (Localisation) ON Dept TO useroracle1;
+
+--9---------------------------------------------------------------------------------
+CONNECT useroracle1/uoracle1; -- Se connecter avec useroracle1
+
+--La modification de la localisation fonctionne car useroracle1 a reçu le privilège spécifique pour cette colonne
+UPDATE useroracle2.Dept SET Localisation = 'Casablanca' WHERE Localisation = 'rabat';
+
+-- La modification du nom de département échoue car useroracle1 n'a pas reçu de privilège UPDATE sur la colonne NomDept
+UPDATE useroracle2.Dept SET NomDept = 'achat' WHERE NomDept = 'ventes';
+
+
+--10---------------------------------------------------------------------------------
+CREATE ROLE developpeur;
+GRANT CREATE TABLE, CREATE VIEW, CREATE SEQUENCE TO developpeur;
+
+--11---------------------------------------------------------------------------------
+GRANT developpeur TO useroracle2;
