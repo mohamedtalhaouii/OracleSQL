95de46a0dd5ca0ea7e051b44720ec97d1cea870b
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2025-04-03 13:22:57 +0000

    ajt le tp2

---
+++ TP2/EX1.sql
@@ -0,0 +1,27 @@
+CREATE TABLE Auteurs(
+    id_auteur NUMBER PRIMARY KEY,
+    nom VARCHAR2(50) NOT NULL,
+    prenom VARCHAR2(50) NOT NULL,
+    date_naissance DATE CHECK(date_naissance < TO_DATE('01-JAN-2000', 'DD-MON-YYYY')),
+    email VARCHAR2(100) UNIQUE,
+    CONSTRAINT FK_id_auteur FOREIGN KEY(id_auteur)
+        REFERENCES Livres(id_auteur)
+);
+
+CREATE TABLE Livres(
+    id_livre NUMBER PRIMARY KEY,
+    titre VARCHAR2(50),
+    id_auteur NUMBER,
+    date_publication DATE,
+    genre VARCHAR2(50) CHECK(genre IN ('Roman', 'Science-fiction', 'Fantasy')),
+    CONSTRAINT FK_id_livre FOREIGN KEY(id_livre)
+        REFERENCES Emprunts(id_livre)
+);
+
+CREATE TABLE Emprunts(
+    id_emprunt NUMBER PRIMARY KEY,
+    id_livre NUMBER,
+    date_emprunt DATE DEFAULT SYSDATE, -- SYSDATE : retourne la date et l'heure actuelles du système
+    date_retour DATE,
+    CONSTRAINT chk_date_retour CHECK (date_retour > date_emprunt)
+);
